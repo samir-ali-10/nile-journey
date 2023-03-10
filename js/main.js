46dee385beba949f1a27e10261422ec0a754044b
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-03-10 15:53:33 +0200

    Password appearance

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let passEye = document.querySelector(".eye");
+let passEyeSlash = document.querySelector(".eye_slash");
+
+let password = document.querySelector("input[type=password]");
+
+
+passEyeSlash.addEventListener("click", () => {
+    passEye.style.display = "block";
+    passEyeSlash.style.display = "none";
+
+    password.type = "text";
+})
+
+
+passEye.addEventListener("click", () => {
+    passEye.style.display = "none";
+    passEyeSlash.style.display = "block";
+
+    password.type = "password";
+})
