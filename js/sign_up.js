635881c79ef7282f50b0d0ffcbbb6c95be3def41
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-03-10 16:45:02 +0200

    Small Edits

--- js/sign_up.js
+++ js/sign_up.js
@@ -2,13 +2,22 @@ let password = document.querySelector(".password");
 let confirmPassword = document.querySelector(".conf_pass");
 let submit = document.querySelector("input[type=submit]");
 
-let inputs = document.querySelectorAll("input");
+let form = document.querySelector("form");
 
 let error = document.querySelector(".error");
 
 submit.addEventListener("click", () => {
-    if(password != confirmPassword) {
+    if (password.value != confirmPassword.value) {
         error.innerHTML = "Passwords doesn't match";
     }
 })
+
+function mySubmitFunction(e) {
+    if (password.value != confirmPassword.value) {
+        e.preventDefault();
+        someBug();
+        return false;
+    }
+
+}
 
